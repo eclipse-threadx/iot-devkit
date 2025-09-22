9eb9f8a6af6e89da6cc2b0288959c71975509ada
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2025-09-22 13:57:16 +0200

    Added MQTT thread, queue, and event flag to main().

--- MXChip/AZ3166/app/main.c
+++ MXChip/AZ3166/app/main.c
@@ -19,6 +19,7 @@
 
 #include "board_init.h"
 #include "cmsis_utils.h"
+#include "mqtt_client.h"
 #include "screen.h"
 #include "sntp_client.h"
 #include "telemetry.h"
@@ -28,14 +29,29 @@
 
 #define ECLIPSETX_THREAD_STACK_SIZE 4096
 #define ECLIPSETX_THREAD_PRIORITY   4
+#define BYTE_POOL_SIZE              1024
+#define QUEUE_SIZE                  16
 
+
+TX_QUEUE mqtt_queue;
+TX_EVENT_FLAGS_GROUP mqtt_app_flag;
+TX_BYTE_POOL byte_pool;
 TX_THREAD telemetry_thread;
+TX_THREAD mqtt_thread;
 ULONG telemetry_thread_stack[ECLIPSETX_THREAD_STACK_SIZE / sizeof(ULONG)];
-
+ULONG mqtt_thread_stack[ECLIPSETX_THREAD_STACK_SIZE / sizeof(ULONG)];
+TX_EVENT_FLAGS_GROUP mqtt_app_flag;
 
 void tx_application_define(void* first_unused_memory)
 {
     systick_interval_set(TX_TIMER_TICKS_PER_SECOND);
+    CHAR *pointer;
+
+    /* Allocate the message queue. */
+    tx_byte_allocate(&byte_pool, (VOID **)&pointer, QUEUE_SIZE*sizeof(ULONG), TX_NO_WAIT);
+
+    /* Create the message queue shared by the telemetry and MQTT threads */
+    tx_queue_create(&mqtt_queue, "Shared queue", TX_1_ULONG, pointer, QUEUE_SIZE*sizeof(ULONG));
 
     // Create Telemetry thread
     UINT status = tx_thread_create(&telemetry_thread,
@@ -53,7 +69,23 @@ void tx_application_define(void* first_unused_memory)
     {
         printf("ERROR: Eclipse ThreadX telemetry thread creation failed\r\n");
     }
-}
+
+    // Create MQTT thread
+    status = tx_thread_create(&mqtt_thread,
+        "Eclipse ThreadX MQTT Thread",
+        mqtt_thread_entry,
+        0,
+        mqtt_thread_stack,
+        ECLIPSETX_THREAD_STACK_SIZE,
+        ECLIPSETX_THREAD_PRIORITY,
+        ECLIPSETX_THREAD_PRIORITY,
+        TX_NO_TIME_SLICE,
+        TX_AUTO_START); 
+
+    if (status != TX_SUCCESS)
+    {
+        printf("ERROR: Eclipse ThreadX MQTT thread creation failed\r\n");
+    }}
 
 int main(void)
 {
